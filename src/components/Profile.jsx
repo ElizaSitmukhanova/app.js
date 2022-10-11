446bb92ac4b48b49b5b2a6dc3a36ba558416e932
ElizaSitmukhanova <eliza.sit@mail.ru>
2022-10-11 15:53:34 +0300

    thirteenth

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+        </div>
+        <div>
+            ava + discription
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>;
+}
+export default Profile;
