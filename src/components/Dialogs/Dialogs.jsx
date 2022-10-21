0b0517477d4390e29de76252a3ee3a19662b25da
ElizaSitmukhanova <eliza.sit@mail.ru>
2022-10-21 15:21:38 +0300

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,45 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Eliza
+                </div>
+                <div className={s.dialog}>
+                    Timur
+                </div>
+                <div className={s.dialog}>
+                    Venera
+                </div>
+                <div className={s.dialog}>
+                    Alisa
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Zoya
+                </div>
+            </div>
+          
+         <div className={s.messages}>
+        
+             <div className={s.message}>
+                  Hi
+             </div>
+             <div className={s.message}>
+                 How are you?
+             </div>
+             <div className={s.message}>
+                 dfhdfdfh
+             </div>
+             <div className={s.message}>
+                dfhgdhdf
+             </div>
+             
+         </div>
+      </div>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium ' />
+        <div className={s.logo}>
+            ES
+        </div>
     </header>
 }
 export default Header;
