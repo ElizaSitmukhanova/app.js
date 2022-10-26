338478e96beee094b663b6cfb0661a27eb04093b
ElizaSitmukhanova <eliza.sit@mail.ru>
2022-10-26 15:44:06 +0300

    MapDialogItem

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,65 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}> {props.name} </NavLink>
-    </div>
-}
-const Messages = (props) => {
-    return <div className={s.dialog}>
-        {props.message}
-    </div>
-}
-
+import DialogItem from './DialogItem/DialogItem'
+import Message from './Message/Message';
+ 
 
 const Dialogs = (props) => {
-    let dialogsData = [ 
-        {id: 1, name: "Eliza"},
-        {id: 2, name: "Monica"}, 
-        {id: 3, name: "Ross"}, 
-        {id: 4, name: "Chandler"}, 
-        {id: 5, name: "Phoebe"}, 
-        {id: 6, name: "Rachel"}, 
-        {id: 7, name: "Joey"}, 
-    ] 
- 
-    let messagesData = [ 
-        {id: 1, message: "Кстати, в качестве благодарности я хочу тебя куда-нибудь сводить."},
-        {id: 2, message: "Серьезно?"},
-        {id: 3, message: "Да. Мы с Джо как раз сегодня идем в кино. Пойдешь с нами? "},
-        {id: 4, message: "Не могу, потому что... я их видела."},
-        {id: 5, message: "Ты видела все фильмы?"},
-        {id: 6, message: "Да! Я большая поклонница... фильмов... кинематографа... кино. "},
-        {id: 7, message: "Может, сходим куда-нибудь еще?"},
-        {id: 8, message: "В зависимости от того, куда. Я почти везде была. "},
-    ] 
- 
-    return ( 
-        <div className={s.dialogs}> 
-            <div className={s.dialogsItems}> 
-            <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id ={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id ={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id ={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id ={dialogsData[5].id}/>
-                <DialogItem name={dialogsData[6].name} id ={dialogsData[6].id}/>
-                 
-            </div> 
-            <div className={s.messages}> 
-            Messages message={messagesData[0].message} id={messagesData[0].id} />
-                <Messages message={messagesData[1].message} id={messagesData[1].id} />
-                <Messages message={messagesData[2].message} id={messagesData[2].id} />
-                <Messages message={messagesData[3].message} id={messagesData[3].id} />
-                <Messages message={messagesData[4].message} id={messagesData[4].id} />
-                <Messages message={messagesData[5].message} id={messagesData[5].id} />
-                <Messages message={messagesData[6].message} id={messagesData[6].id} />
-                <Messages message={messagesData[7].message} id={messagesData[7].id} />
-            </div> 
-        </div> 
+     
+    let dialogsData = [
+        { id: 1, name: "Eliza" },
+        { id: 2, name: "Monica" },
+        { id: 3, name: "Ross" },
+        { id: 4, name: "Chandler" },
+        { id: 5, name: "Phoebe" },
+        { id: 6, name: "Rachel" },
+        { id: 7, name: "Joey" },
+    ];
+
+    let messagesData = [
+            { id: 1, message: "Кстати, в качестве благодарности я хочу тебя куда-нибудь сводить." },
+            { id: 2, message: "Серьезно?" },
+            { id: 3, message: "Да. Мы с Джо как раз сегодня идем в кино. Пойдешь с нами? " },
+            { id: 4, message: "Не могу, потому что... я их видела." },
+            { id: 5, message: "Ты видела все фильмы?" },
+            { id: 6, message: "Да! Я большая поклонница... фильмов... кинематографа... кино. " },
+            { id: 7, message: "Может, сходим куда-нибудь еще?" },
+            { id: 8, message: "В зависимости от того, куда. Я почти везде была. " },
+          ];
+
+
+    let DialogElement =
+        dialogsData.map(d => <DialogItem name={d.name} id={d.id} />)
+
+    let MessagesElement =
+        messagesData.map(m => <Message message={m.message} id={m.id} />)
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {DialogElement}
+            </div>
+            <div className={s.messages}>
+                {MessagesElement}
+            </div>
+        </div>
     )
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 
 const MyPosts = () => {
     let postData = [
-        {id: 1, message: 'Hello, my name is Eliza.And you?', image:'https://sun9-34.userapi.com/impf/c854416/v854416044/c61ae/7G4AJo-AM5s.jpg?size=960x1200&quality=96&sign=f7064faaa607729de77ec3aef7383dc5&type=album', likesCount: 0},
-        {id: 2, message: 'Hi, i am  Rachel .Nice to meet you! Where are you from?', image:'https://art-assorty.ru/wp-content/uploads/2018/11/23256354753-1.jpg', likesCount: 25},
+        { id: 1, message: 'Hello, my name is Eliza.And you?', image: 'https://sun9-34.userapi.com/impf/c854416/v854416044/c61ae/7G4AJo-AM5s.jpg?size=960x1200&quality=96&sign=f7064faaa607729de77ec3aef7383dc5&type=album', likesCount: 0 },
+        { id: 2, message: 'Hi, i am  Rachel .Nice to meet you! Where are you from?', image: 'https://art-assorty.ru/wp-content/uploads/2018/11/23256354753-1.jpg', likesCount: 25 },
     ];
+
+    let postElements =
+        postData.map(p => <Post message={p.message} likesCount={p.likesCount} image={p.image} />)
     return (
         <div className={s.postsBlock}>
-            <h3> My posts </h3> 
+            <h3> My posts </h3>
             <div>
                 <div>
                     <textarea> </textarea>
@@ -19,8 +22,7 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount} image={postData[0].image} />
-                <Post message={postData[1].message} likesCount={postData[1].likesCount} image={postData[1].image}/>
+                {postElements}
             </div>
         </div>
     )
