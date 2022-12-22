85e801cc2b2d233b33c002e4a1939b754872c7e0
ElizaSitmukhanova <eliza.sit@mail.ru>
2022-12-22 20:11:02 +0300

    reducerwitch

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,3 +1,7 @@
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
@@ -5,7 +9,7 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const UPDATE_NEW_MESSAGE_BODY =  'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-
+ 
 let store = {
   _state: {
     profilePage: {
@@ -39,6 +43,12 @@ let store = {
     },
     sideBar: {
       sideData: []
+    },
+    newsPage: {
+      newsData: []
+    },
+    musicPage: {
+      musicData: []
     }
 
 
@@ -55,30 +65,14 @@ let store = {
  
   
   dispatch(action) {
-    if (action.type === ADD_POST) {
-      let newPost = {
-        id: 3,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-        image: 'https://art-assorty.ru/wp-content/uploads/2018/11/23256354753-1.jpg',
-      };
-        this._state.profilePage.postData.push(newPost);
-        this._state.profilePage.newPostText = "";
-        this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        this._state.profilePage.newPostText = action.newText;
-        this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        this._state.dialogsPage.newMessageBody = action.body;
-        this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-        let body = this._state.dialogsPage.newMessageBody;
-        this._state.dialogsPage.newMessageBody = '';
-        this._state.dialogsPage.messagesData.push({ id: 9, message: body});
-        this._callSubscriber(this._state);
-  }
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.sideBar = sidebarReducer(this._state.sidebarPage, action);
+    
+    this._callSubscriber(this._state);
+    
 }
-}
+};
 
 export const addPostActionCreator = () => ({ type: ADD_POST});
 export const updateNewPostTextActionCreator = (text) => 
